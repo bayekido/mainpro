d1f8ade43468583674f47bfedae2aa8b982baed1
bayekido <osanakidane@gmail.com>
2023-11-04 01:32:20 +0300

    first project

---
+++ Person.java
@@ -0,0 +1,46 @@
+public class Person {
+    private int id;
+    private String name;
+    private int age;
+    private boolean parentPermission;
+
+    public Person(int age, String name, boolean parentPermission) {
+        this.id = generateId();
+        this.name = name;
+        this.age = age;
+        this.parentPermission = parentPermission;
+    }
+
+    private int generateId() {
+        // Assume implementation for generating a unique ID
+        return 0;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    private boolean isOfAge() {
+        return age >= 18;
+    }
+
+    public boolean canUseServices() {
+        return isOfAge() || parentPermission;
+    }
+}
